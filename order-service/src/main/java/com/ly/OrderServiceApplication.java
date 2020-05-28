6f0dd672c21ab0d428d0e1f58313b8578d2a13a7
lmy1965673628 <1965673628@qq.com>
2020-05-28 10:37:16 +0800

    order service 调用商品库存服务和用户服务

--- order-service/src/main/java/com/ly/OrderServiceApplication.java
+++ order-service/src/main/java/com/ly/OrderServiceApplication.java
@@ -1,11 +1,11 @@
 package com.ly;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
-@SpringBootApplication
+@EnableFeignClients
+@SpringCloudApplication
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/com/ly/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/ly/service/OrderServiceImpl.java
@@ -1,24 +1,49 @@
 package com.ly.service;
 
+import com.ly.feignclient.ItemFeignService;
+import com.ly.feignclient.UserFeignService;
+import com.ly.pojo.Item;
 import com.ly.pojo.Order;
+import com.ly.pojo.User;
+import com.ly.util.JsonResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private ItemFeignService itemService;
+    @Autowired
+    private UserFeignService userService;
+
     @Override
     public Order getOrder(String orderId) {
-        //TODO: 调用user-service获取用户信息
-        //TODO: 调用item-service获取商品信息
+        //调用user-service获取用户信息
+        JsonResult<User> user = userService.getUser(7);
+
+        //调用item-service获取商品信息
+        JsonResult<List<Item>> items = itemService.getItems(orderId);
+
+
         Order order = new Order();
         order.setId(orderId);
+        order.setUser(user.getData());
+        order.setItems(items.getData());
         return order;
     }
+
     @Override
     public void addOrder(Order order) {
-        //TODO: 调用item-service减少商品库存
+        //调用item-service减少商品库存
+        itemService.decreaseNumber(order.getItems());
+
         //TODO: 调用user-service增加用户积分
+        userService.addScore(7, 100);
+
         log.info("保存订单："+order);
     }
 }
